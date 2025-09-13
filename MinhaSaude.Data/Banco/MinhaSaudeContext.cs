d48fb18943c9d1775a9ea2d48b7c475b58e6067c
Lucas Gomes Santos <lgsantos306@gmail.com>
2025-09-13 17:49:30 -0300

    Criado DAL e Context

---
+++ MinhaSaude.Data/Banco/MinhaSaudeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinhaSaude.Data.Banco;
+
+public class MinhaSaudeContext : DbContext
+{
+
+    private string connectionString = "Data Source=NIKKI\\SQLEXPRESS;Initial Catalog=MinhaSaude;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured) return;
+
+        optionsBuilder
+            .UseSqlServer(connectionString)
+            .UseLazyLoadingProxies();
+    }
+}
